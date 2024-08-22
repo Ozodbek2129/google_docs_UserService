f3fa0177e6b4ca69878e37dce098478ed16ef25f
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-08-22 13:21:10 +0500

    Ready

--- storage/postgres/user_crud.go
+++ storage/postgres/user_crud.go
@@ -2,9 +2,14 @@ package postgres
 
 import (
 	"database/sql"
+	"google_docs_user/genproto/user"
+	"google_docs_user/models"
 	"google_docs_user/pkg/logger"
 	"google_docs_user/storage"
+	"google_docs_user/storage/redis"
 	"log/slog"
+
+	"golang.org/x/net/context"
 )
 
 type UserRepository struct {
@@ -16,4 +21,21 @@ func NewUserRepository(db *sql.DB) storage.IUserStorage {
 	return &UserRepository{Db: db, Log: logger.NewLogger()}
 }
 
-func 
+func (u *UserRepository) ConfirmationRegister(ctx context.Context, req *user.C) (*user.RegisterRes, error){
+
+	res, err := redis.GetUser(ctx, req.Email)
+	if err == nil {
+        u.Log.Error("User already exists with email: %s", req.Email)
+        return nil, err
+    }
+
+	req1 := models.Register{
+		Email:     req.Email,
+        Password:  req.Password,
+        FirstName: req.FirstName,
+        LastName:  req.LastName,
+		Code: res.Code,
+	}
+
+
+}
--- storage/redis/redis.go
+++ storage/redis/redis.go
@@ -1,9 +1,11 @@
 package redis
 
 import (
-	"google_docs_user/config"
 	"context"
+	"encoding/json"
 	"fmt"
+	"google_docs_user/config"
+	"google_docs_user/models"
 	"time"
 
 	"github.com/pkg/errors"
@@ -26,7 +28,7 @@ func StoreCode(ctx context.Context, email, code string) error {
 	fmt.Println("Storing code for email: " + email + " with code: " + code)
 	rdb := ConnectDB()
 
-	err := rdb.Set(ctx, "health:"+email, code, time.Minute*3).Err()
+	err := rdb.Set(ctx, email, code, time.Minute*3).Err()
 	if err != nil {
 		return errors.Wrap(err, "failed to store code in redis")
 
@@ -38,7 +40,7 @@ func StoreCode(ctx context.Context, email, code string) error {
 func GetCode(ctx context.Context, email string) (string, error) {
 	rdb := ConnectDB()
 
-	code, err := rdb.Get(ctx, "health:"+email).Result()
+	code, err := rdb.Get(ctx, email).Result()
 	if err != nil {
 		if err == redis.Nil {
 			return "", errors.New("code not found for " + email)
@@ -52,7 +54,7 @@ func GetCode(ctx context.Context, email string) (string, error) {
 func DeleteCode(ctx context.Context, email string) error {
 	rdb := ConnectDB()
 
-	err := rdb.Del(ctx, "health:"+email).Err()
+	err := rdb.Del(ctx, email).Err()
 	if err != nil {
 		return errors.Wrap(err, "failed to delete code from redis")
 	}
@@ -60,6 +62,39 @@ func DeleteCode(ctx context.Context, email string) error {
 	return nil
 }
 
-func RegisterUser(ctx context.Context, email, code string, ) error {
+func RegisterUser(ctx context.Context, code string, req models.Register) error {
+	rdb := ConnectDB()
 
+	res, err := json.Marshal(req)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal user data")
+	}
+
+	err = rdb.Set(ctx, req.Email, res, time.Minute*10).Err()
+	if err != nil {
+		return errors.Wrap(err, "failed to store user data in redis")
+	}
+
+	return nil
+}
+
+
+func GetUser(ctx context.Context, email string) (models.Register, error) {
+	rdb := ConnectDB()
+
+    res, err := rdb.Get(ctx, email).Result()
+    if err!= nil {
+        if err == redis.Nil {
+            return models.Register{}, errors.New("user not found for " + email)
+        }
+        return models.Register{}, errors.Wrap(err, "failed to get user from redis")
+    }
+
+    var user models.Register
+    err = json.Unmarshal([]byte(res), &user)
+    if err!= nil {
+        return models.Register{}, errors.Wrap(err, "failed to unmarshal user data")
+    }
+
+    return user, nil
 }
