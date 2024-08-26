d84f2468c4a546e241c635f4f6d058ce4b73fdae
Sanjarbek <sanjarbekabdurahmonov123@gmail.com>
2024-08-26 18:40:12 +0500

    Ready

--- models/models.go
+++ models/models.go
@@ -1,9 +1,15 @@
 package models
 
+import "mime/multipart"
+
 type Register struct {
 	Email     string `json:"email"`
 	Password  string `json:"password"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	// Code      int64 `json:"code"`
+}
+
+type File struct {
+	File multipart.FileHeader `form:"file" binding:"required"`
 }
